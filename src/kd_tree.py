beb343aa9594a1d63cdb430418e95f5c1f103af9
nickastonge <nstonge@ufl.edu>
2026-07-11 13:36:10 -0400

    Added comments on k-d tree for readability

--- src/kd_tree.py
+++ src/kd_tree.py
@@ -11,13 +11,18 @@ class Node:
     right: Optional["Node"] = None
 
 def build_tree(tracks, depth=0):
+# Input: matrix of song tracks paired with their index -> (index, [features, ...])
+# Output: root node of the completed k-d tree
 
     if not tracks:
         return None
 
+    # derives k dimensions from the number of features
+    # axis cycles with depth
     k = len(tracks[0][1])
     axis = depth % k
 
+    # sorts tracks by current axis and finds midpoint
     tracks = sorted(tracks, key=lambda track: track[1][axis])
     mid = len(tracks) // 2
     i, f = tracks[mid]
@@ -26,11 +31,15 @@ def build_tree(tracks, depth=0):
         index = i,
         features = f,
         axis = axis,
+
+        # recursively builds the rest of the tree
         left = build_tree(tracks[:mid], depth+1),
         right = build_tree(tracks[mid+1:], depth+1)
     )
 
 def distance(features1, features2):
+# Input: list of feature statistics from two different tracks
+# Output: sum of squared distances between each feature
 
     result = 0
     for i in range(len(features1)):
@@ -39,6 +48,10 @@ def distance(features1, features2):
     return result
 
 def nearest(root, target, best=None):
+# Input: root node of the tree,
+#        list of target feature statistics,
+#        current nearest neighbor identified
+# Output: nearest node to target
 
     if not root:
         return best
@@ -46,7 +59,9 @@ def nearest(root, target, best=None):
     if not best or distance(root.features, target) < distance(target, best.features):
         best = root
 
+    # determine which axis is being used
     axis = root.axis
+    # determine which side of the tree is near vs far
     if target[axis] < root.features[axis]:
         near = root.left
         far = root.right
@@ -54,8 +69,10 @@ def nearest(root, target, best=None):
         near = root.right
         far = root.left
 
+    # recursive search for nearest neighbor
     best = nearest(near, target, best)
 
+    # determine if other side of the tree could have a closer child node
     if (target[axis] - root.features[axis]) ** 2 < distance(best.features, target):
         best = nearest(far, target, best)
 
